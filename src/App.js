69d4211ccd877d950f3b43d05fa310baf5409f0d
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2020-02-04 14:59:36 -0500

    adds Input and Ouput components and adds them to the App class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from './Input';
+import Ouput from './Output';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  
+  
+  render(){
+    return (
+      <div className= 'App'>
+        <Input />
+        <Ouput />
+      </div>
+    )
+  }
+
 }
+
 
 export default App;
---
+++ src/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Input (){
+    return (
+        <label>
+            Type Here!
+        </label>
+    )
+}
+
+export default Input;
